394b088608f2ac9699887bf0ee0d7aee151ef141
chess-equality <chessequality@gmail.com>
2021-08-23 12:19:22 +0800

    Start query time series

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,13 @@ mod entities;
 
 pub use crate::connector_components::*;
 
+use crate::format::Format;
+use crate::locations::Locations;
+use crate::optionals::Optionals;
+use crate::parameters::Parameters;
+use crate::valid_date_time::ValidDateTime;
+use chrono::Utc;
+
 #[macro_use]
 extern crate derive_builder;
 extern crate linked_hash_set;
@@ -23,8 +30,104 @@ impl MeteomaticsConnector {
         }
     }
 
-    pub async fn query_time_series(&self, url_fragment: &str) -> Result<Response, reqwest::Error> {
-        let response = self.api_client.do_http_get(url_fragment).await?;
+    pub async fn query_time_series(
+        &self,
+        vdt: ValidDateTime<Utc>,
+        parameters: Parameters<'_>,
+        locations: Locations<'_>,
+        optionals: Optionals<'_>,
+    ) -> Result<Response, reqwest::Error> {
+        let url_fragment = format!(
+            "{}--{}/{}/{}/{}?{}",
+            vdt.start_date_time.to_rfc3339(),
+            vdt.end_date_time.unwrap().to_rfc3339(),
+            parameters,
+            locations,
+            Format::CSV.to_string(),
+            optionals
+        );
+        let response = self.api_client.do_http_get(url_fragment.as_str()).await?;
         Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::locations::{Coordinates, Locations, LocationsBuilder};
+    use crate::optionals::OptionalsBuilder;
+    use crate::optionals::{Opt, OptSet, Optionals};
+    use crate::parameters::{PSet, Parameters, ParametersBuilder, P};
+    use crate::valid_date_time::{ValidDateTime, ValidDateTimeBuilder};
+    use crate::MeteomaticsConnector;
+    use chrono::{Duration, Utc};
+    use std::iter::FromIterator;
+
+    #[tokio::test]
+    async fn call_query_time_series() {
+        let meteomatics_connector = MeteomaticsConnector::new(
+            "python-community".to_string(),
+            "Umivipawe179".to_string(),
+            10,
+        );
+
+        let now = Utc::now();
+        let yesterday = now.clone() - Duration::days(1);
+        let utc_vdt: ValidDateTime<Utc> = ValidDateTimeBuilder::default()
+            .start_date_time(yesterday)
+            .end_date_time(now)
+            .build()
+            .unwrap();
+
+        let p_values: PSet<'_> = PSet::from_iter([
+            P {
+                k: "t_2m",
+                v: Some("C"),
+            },
+            P {
+                k: "precip_1h",
+                v: Some("mm"),
+            },
+        ]);
+        let parameters: Parameters = ParametersBuilder::default()
+            .p_values(p_values)
+            .build()
+            .unwrap();
+
+        let coordinates = Coordinates::from(["47.419708", "9.358478"]);
+        let locations: Locations = LocationsBuilder::default()
+            .coordinates(coordinates)
+            .build()
+            .unwrap();
+
+        let opt_values: OptSet<'_> = OptSet::from_iter([
+            Opt {
+                k: "source",
+                v: "mix",
+            },
+            Opt {
+                k: "calibrated",
+                v: "true",
+            },
+        ]);
+        let optionals: Optionals = OptionalsBuilder::default()
+            .opt_values(opt_values)
+            .build()
+            .unwrap();
+
+        let response = meteomatics_connector
+            .query_time_series(utc_vdt, parameters, locations, optionals)
+            .await
+            .unwrap();
+
+        let status = format!("{}", response.status());
+        println!("Status: {}", status);
+        println!("Headers:\n{:#?}", response.headers());
+
+        let body = response.text().await.unwrap();
+        println!("Body:\n{}", body);
+
+        assert_eq!(status, "200 OK");
+        assert_ne!(body, "");
     }
 }
