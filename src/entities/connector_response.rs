4f219044bb02a22b8235cede93a08a22f2e5a1d1
chessequality <chessequality@gmail.com>
2021-08-29 17:36:21 +0800

    Started CSV response processing

--- src/entities/connector_response.rs
+++ src/entities/connector_response.rs
@@ -1,9 +1,61 @@
-use crate::entities::connector_error::ConnectorError;
+use crate::connector_error::ConnectorError;
+use csv::Reader;
+use std::io::Read;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct ConnectorResponse {
-    body: String,
-    error: ConnectorError,
+    pub body: CSVBody,
+    pub http_status: String,
+    pub error: ConnectorError,
 }
 
-impl ConnectorResponse {}
+pub type CSVHeader = Vec<String>;
+pub type CSVRecord = Vec<Vec<String>>;
+
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct CSVBody {
+    pub csv_headers: CSVHeader,
+    pub csv_records: CSVRecord,
+}
+
+impl CSVBody {
+    #![allow(unused_mut)]
+    pub fn new() -> Self {
+        let mut csv_headers: CSVHeader = Default::default();
+        let mut csv_records: CSVRecord = Default::default();
+        csv_headers.push("validdate".to_owned());
+        Self {
+            csv_headers,
+            csv_records,
+        }
+    }
+
+    pub async fn add_header(&mut self, header: String) {
+        self.csv_headers.push(header);
+    }
+
+    pub async fn populate_records<R: Read>(
+        &mut self,
+        rdr: &mut Reader<R>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        for record in rdr.records() {
+            let record = record?;
+            let mut row: Vec<String> = vec![];
+            for (&_, value) in self.csv_headers.iter().zip(record.iter()) {
+                row.push(value.to_owned());
+            }
+            self.csv_records.push(row);
+        }
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for CSVBody {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self.csv_headers.to_vec().join(","))?;
+        for row in self.csv_records.iter() {
+            writeln!(f, "{}", row.to_vec().join(","))?;
+        }
+        Ok(())
+    }
+}
