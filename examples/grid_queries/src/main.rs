5f43755e9f7b16602af4bf95a5e5a6bd8387842e
Bastian Buman <bbuman@meteomatics.com>
2022-05-25 11:29:12 +0200

    Add documentation, use dotenv

--- examples/grid_queries/src/main.rs
+++ examples/grid_queries/src/main.rs
@@ -1,14 +1,31 @@
-use chrono::{Utc, DateTime};
+//! # Grid Query 
+//! This is a demonstration of how to download a DataFrame for an area or "grid" defined by a bounding-box 
+//! and a grid resolution (e.g. 0.1 °). 
+//! To run the example either change ```u_name``` and ```u_pw```
+//! directly *or* create a file called ```.env``` and put the following lines in there:
+//! ```text
+//! METEOMATICS_PW=your_password
+//! METEOMATICS_USER=your_username
+//! ```
+//! Make sure to include ```.env``` in your ```.gitignore```. This is a safer variant for developers 
+//! to work with API credentials as you will never accidentally commit/push your credentials.
+
+use chrono::{Utc};
 use rust_connector_api::APIClient;
 use rust_connector_api::location::BBox;
 use rust_connector_api::errors::ConnectorError;
 use polars::prelude::*;
+use std::env;
+use dotenv::dotenv;
 
+// Demonstrates how to use the rust connector to query the Meteomatics API for gridded data. Also 
+// demonstrates how to work with the resulting ```DataFrame```.
 #[tokio::main]
 async fn main(){
-    // Credentials  
-    let u_name: String = String::from("python-community");
-    let u_pw: String = String::from("Umivipawe179");
+    // Credentials
+    dotenv().ok();
+    let u_pw: String = env::var("METEOMATICS_PW").unwrap();
+    let u_name: String = env::var("METEOMATICS_USER").unwrap();
 
     // Create Client
     let api: APIClient = APIClient::new(u_name,u_pw,10);
@@ -34,10 +51,10 @@ async fn main(){
     }
 }
 
-/// Query a time series for a single point and two parameters.
+/// Demonstrates how to query a time series for a single point in time (now), a grid and two parameters.
 async fn example_request(api: &APIClient) -> std::result::Result<DataFrame, ConnectorError>{
     // Time series definition
-    let start_date: DateTime<Utc> = Utc::now();
+    let start_date = Utc::now();
 
     // Location definition
     let ch: BBox = BBox {
@@ -50,13 +67,13 @@ async fn example_request(api: &APIClient) -> std::result::Result<DataFrame, Conn
     };
 
     // Parameter selection
-    let param1: String = String::from("t_2m:C");
-    let param2: String = String::from("precip_1h:mm");
-    let params: Vec<String> = vec![param1, param2];
+    let temp2m = String::from("t_2m:C");
+    let precip1h = String::from("precip_1h:mm");
+    let params = vec![temp2m, precip1h];
 
     // Optionals
-    let opt1: String = String::from("model=mix");
-    let optionals: Option<Vec<String>> = Option::from(vec![opt1]);
+    let model_mix = String::from("model=mix");
+    let optionals = Option::from(vec![model_mix]);
     
     let result = api.query_grid_unpivoted(
         &start_date, &params, &ch, &optionals
